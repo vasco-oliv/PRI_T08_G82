23334b38b9caf78de47504efda0b64f49a39a9e8
José Luiz <ZLAmoedo@gmail.com>
2024-11-07 17:02:56 +0000

    Script to send schema and documents to solr

---
+++ startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script expects a container started with the following command.
+# docker run -p 8983:8983 --name meic_solr -v ${PWD}:/data -d solr:9 solr-precreate posts
+
+#Schema definition via API
+curl -X POST -H 'Content-type:application/json' \
+    --data-binary '@data/solr_schema.json' \
+    http://localhost:8983/solr/posts/schema
+
+
+# Populate collection using mapped path inside container.
+docker exec -it meic_solr bin/solr post -c posts ../../data/Dataset/dataset.json
